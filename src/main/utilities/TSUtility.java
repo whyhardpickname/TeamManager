55b6547a0b812985a08c7bdb972909cb427777b7
machern <13zxchen3@gmail.com>
2021-01-29 08:10:44 +0800

    编写测试读取键盘输入的测试用例

--- src/main/utilities/TSUtility.java
+++ src/main/utilities/TSUtility.java
@@ -1,11 +1,17 @@
 package utilities;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 /**
  * {@code TSUtility}类提供实现键盘访问的方法。
  * @author MarkChern
  */
 public class TSUtility
 {
+    /** 读取用户输入 */
+    private static Scanner scanner = new Scanner(System.in);
     /**
      * {@code readMenuSelection}方法读取键盘，如果用户键入’1’-’4’中的任意字符，则方法返回。
      * 返回值为用户键入字符。
@@ -40,5 +46,64 @@ public class TSUtility
     public static char readConfirmSelection()
     {
         return 'N';
+    }
+
+    /**
+     * {@code readKeyBoard}方法从键盘读取最长{@code limit}的输入。
+     * @param limit
+     *      最长输入
+     * @param blankReturn
+     *      换行标识
+     * @return
+     *      读取的行
+     */
+    private String readKeyBoard(int limit, boolean blankReturn)
+    {
+        String line = "";
+        while (scanner.hasNextLine())
+        {
+            line = scanner.nextLine();
+            if (line.length() == 0)
+            {
+                if (blankReturn)
+                {
+                    return line;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (line.length() < 1 || line.length() > limit)
+            {
+                System.out.println("输入长度错误，请重新输入：");
+                continue;
+            }
+            break;
+        }
+        return  line;
+    }
+
+    public static void simulateInput()
+    {
+        InputStream backup = System.in;
+        ByteArrayInputStream input = new ByteArrayInputStream("1".getBytes());
+        System.setIn(input);
+        if (scanner.nextInt() == 1)
+        {
+            System.out.println("ok");
+        }
+        else
+        {
+            System.out.println("not ok");
+        }
+
+        System.setIn(backup);
+    }
+
+    public static void main(String[] args)
+    {
+        simulateInput();
     }
 }
--- src/test/java/utilities/TSUtilityTest.java
+++ src/test/java/utilities/TSUtilityTest.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,11 +21,40 @@ class TSUtilityTest
 {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
+    private  ByteArrayInputStream in;
+
+    @BeforeEach
+    void setUp()
+    {
+
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+
+    }
 
     @Test
     void readMenuSelection()
     {
+        //备份原输入输出
+        InputStream sysBackup = System.in;
+        PrintStream originalOut = System.out;
+        //模拟输入,保存输出
+        String simulatedUserInput = "0" + System.getProperty("line.separator") + "1";
+        in = new ByteArrayInputStream(simulatedUserInput.getBytes());
 
+        System.setIn(in);
+        System.setOut(new PrintStream(outContent));
+
+        assertEquals('0', TSUtility.readMenuSelection());
+        assertEquals("error: enter again.", outContent.toString());
+
+
+        //恢复
+        System.setIn(sysBackup);
+        System.setOut(originalOut);
     }
 
     @Test
@@ -40,5 +72,43 @@ class TSUtilityTest
     @Test
     void readConfirmSelection()
     {
+    }
+
+    @Test
+    void readKeyBoard() throws Exception
+    {
+        Method method =
+                TSUtility.class.getDeclaredMethod("readKeyBoard", int.class, boolean.class);
+
+        method.setAccessible(true);
+        TSUtility utility = new TSUtility();
+
+        //备份原输入输出
+        InputStream sysBackup = System.in;
+        PrintStream originalOut = System.out;
+
+        //空白输入+ false + 正确输入
+        String simulatedUserInput = "" + System.getProperty("line.separator") + "1" +
+                System.getProperty("line.separator");
+        in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+
+        //模拟输入,保存输出
+        System.setIn(in);
+        System.setOut(new PrintStream(outContent));
+
+        assertEquals("1", method.invoke(utility, 1, false));
+        assertEquals("输入长度错误，请重新输入：", outContent.toString());
+
+        /*System.setIn(in);
+        System.setOut(new PrintStream(outContent));
+        //空白输入+ true
+        simulatedUserInput = "";
+        in = new ByteArrayInputStream(simulatedUserInput.getBytes());
+        assertEquals("", method.invoke(utility, 1, false));
+        assertNull(outContent.toString());*/
+
+        //恢复
+        System.setIn(sysBackup);
+        System.setOut(originalOut);
     }
 }
